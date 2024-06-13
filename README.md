b8fcb0c55b9da7581820b8cb09c4b21cb58b2bdb
Mohammad Hossain <moh.hossain98@gmail.com>
2024-06-13 04:10:52 -0400

    adding responsive css

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+https://dribbble.com/shots/20037874-Landing-Page-Concept
+https://www.canva.com/design/DAGH9k8r9WM/rRyhJ87K5OdgSwtJc0MftQ/edit
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import mockup from '../assets/mockup.png'
+import { FaToggleOn } from "react-icons/fa";
+
 
 function MainSection() {
   return (
     <div className='main-top'>
+        <div className='main-background'>
+            <h1 className='background'>Easy Smart</h1>
+            
+        </div>
         <div className='slogan'> 
             <h1>Control your home with your fingertip</h1>
+            <FaToggleOn className='toggle' />
         </div>
         <div className='mockup'>
             <img width={400} height={500} src={mockup} alt='mockup' />
+        </div>
+
+        <div className='main-bottom'>
+            <p>With tail in the air fight own tail or give attitude. White cat sleeps on a black shirt check cat door for ambush 10 times.</p>
+            <p>Chew the plant catch eat throw up catch eat throw up bad birds. Touch water with paw then recoil in horror eat the fat cats food so touch my tail</p>
         </div>
     </div>
 
